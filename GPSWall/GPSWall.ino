8cd14d2f2b618a82207da5c20e2a8a78175f1bfd
jcahal <jacahal@gmail.com>
2018-04-05 13:30:29 -0700

    changed distance calc

--- GPSWall/GPSWall.ino
+++ GPSWall/GPSWall.ino
@@ -35,23 +35,23 @@ int localkey = 0;
 int iterator = 0; // for printing bearing
 
 ///////////////////////////////////////// Boundary points  //////////////////////////////////////////
-long int latPoint1 = 33.421846 * 100000;     // reference destination (Point1)
-long int lonPoint1 =  -111.934683 * 100000;   // reference destination (Point1)
+long int latPoint1 = 33.421846 * 1000000;     // reference destination (Point1)
+long int lonPoint1 =  -111.934683 * 1000000;   // reference destination (Point1)
 
-long int latPoint2 = 33.421846 * 100000;     // reference destination (Point2)
-long int lonPoint2 =  -111.933382 * 100000;   // reference destination (Point2)
+long int latPoint2 = 33.421846 * 1000000;     // reference destination (Point2)
+long int lonPoint2 =  -111.933382 * 1000000;   // reference destination (Point2)
 
-long int latPoint3 = 33.421147 * 100000;     // reference destination (Point3)
-long int lonPoint3 =  -111.933772 * 100000;   // reference destination (Point3)
+long int latPoint3 = 33.421198 * 1000000;     // reference destination (Point3)
+long int lonPoint3 =  -111.933811 * 1000000;   // reference destination (Point3)
 
-long int latPoint4 = 33.420825 * 100000;     // reference destination (Point4)
-long int lonPoint4 =  -111.933824 * 100000;   // reference destination (Point4)
+long int latPoint4 = 33.420769 * 1000000;     // reference destination (Point4)
+long int lonPoint4 =  -111.933811 * 1000000;   // reference destination (Point4)
 
-long int latPoint5 = 33.420823 * 100000;     // reference destination (Point5)
-long int lonPoint5 =  -111.934168 * 100000;   // reference destination (Point5)
+long int latPoint5 = 33.420769 * 1000000;     // reference destination (Point5)
+long int lonPoint5 =  -111.934175 * 1000000;   // reference destination (Point5)
 
-long int latPoint6 = 33.421151 * 100000;     // reference destination (Point6)
-long int lonPoint6 =  -111.934220 * 100000;   // reference destination (Point6)
+long int latPoint6 = 33.421198 * 1000000;     // reference destination (Point6)
+long int lonPoint6 =  -111.934175 * 1000000;   // reference destination (Point6)
 /////////////////////////////////////////////////////////////////////////////////////////////////////
 
 long int lats[6] = {latPoint1, latPoint2, latPoint3, latPoint4, latPoint5, latPoint6};
@@ -73,7 +73,7 @@ void setup() {
   //byte c_data[22] = {255, 255, 0, 0, 250, 255, 205, 253, 175, 2, 16, 2, 1, 0, 254, 255, 1, 0, 232, 3, 27, 4}; // Brickyard
   
   // Production site
-  byte c_data[22] = {0, 0, 0, 0, 0, 0, 82, 253, 156, 2, 80, 1, 0, 0, 0, 0, 2, 0, 232, 3, 184, 2}; // Old Main
+  byte c_data[22] = {0, 0, 0, 0, 0, 0, 174, 253, 126, 2, 193, 1, 255, 255, 1, 0, 1, 0, 232, 3, 237, 2}; // Old Main
   bno.setCalibData(c_data);                                                                                       // SET CALIBRATION DATA
   bno.setExtCrystalUse(true);
 
@@ -154,12 +154,26 @@ void GPSRead() {
   if (GPS.fix) {
     // read GPS Latitude in degree decimal
     // read GPS Longitude in degree decimal
-    lat = GPS.latitudeDegrees * 100000;
-    lon = GPS.longitudeDegrees * 100000;
+    lat = GPS.latitudeDegrees * 1000000;
+    lon = GPS.longitudeDegrees * 1000000;
     Serial.print("LAT");
     Serial.print(lat);
     Serial.print(" LON");
     Serial.println(lon);
+    Serial.print("DW1-");
+    Serial.print(dists[0]);
+    Serial.print(" DW2-");
+    Serial.print(dists[1]);
+    Serial.print(" DW3-");
+    Serial.print(dists[2]);
+    Serial.print(" DW4-");
+    Serial.print(dists[3]);
+    Serial.print(" DW5-");
+    Serial.print(dists[4]);
+    Serial.print(" DW6-");
+    Serial.print(dists[5]);
+    Serial.print(" Bearing-");
+    Serial.println(bearing);
   } else {
     Serial.println("NO FIX");
   }
@@ -167,35 +181,21 @@ void GPSRead() {
 
 double CalculateDistancePerpendicular(double x1, double  y1, double  x2, double  y2, double  x3, double y3) {       // Function to calculate distance from the wall --- INPUTs:( Current x, Current y, Point i (x). Point i (y), Point j (x), Point j (y) )
   double d;
-  double dX = x3 - x2;
-  double dY = y3 - y2;
-  double m = dY / dX; //slope
-  double b = y2 - m * x2; // y-intercept
-
-  double perpM;
-  double perpB;
-  double poiX;
-  double poiY;
 
   if(y2 == y3) { // Horozontal line
-    perpM = 0;
-    perpB = y2;
-    poiX = x1;
-    poiY = b;
+      d = y2 - y1;
   } else if(x2 == x3) { // Vertical line
-    poiX = x2;
-    poiY = y1;
+      d = x2 - x1;
   } else {
-    perpM = (-1) / m; // perpendicular slope
-    perpB = y1 - (x1 * perpM); // perpendicular y-intercept
-    poiX = (perpB - b) / (m - perpM); //Point of intersection x
-    poiY = (perpM * poiX) + perpB; //Point of intersection y
+      double dX = x3 - x2;
+      double dY = y3 - y2;
+      double m = dY / dX; //slope
+      double b = y2 - (m * x2); // y-intercept
+      double poiY = m * x1 + b;
+      double poiX = (y1 - b) / m;
+      
+      d = poiX - x1;
   }
-
-  double dX2 = poiX - x1;
-  double dY2 = poiY - y1;
- 
-  d = sqrt((dX2 * dX2) + (dY2 * dY2));
   
   return d;                         // The output of this function is distance from the wall
 
@@ -219,10 +219,11 @@ void CalculateDistanceToWalls() {
     if(j == 6) 
       j = 0;
 
-    dist = CalculateDistancePerpendicular(lon, lat, (double)lons[i], (double)lats[i], (double)lons[j], (double)lats[j]);
+    dists[i] = CalculateDistancePerpendicular(lon, lat, (double)lons[i], (double)lats[i], (double)lons[j], (double)lats[j]);
 
-    // Put distance in distances array.
-    dists[i] = dist;
+    if(i == 3) {
+      dists[i] = dists[i] * -1.0;
+    }
     
   }
   
@@ -244,8 +245,6 @@ void CalculateBearing() {
     Serial.println("Hit the wall");
   }
   */
-
-  iterator++;
   
   // Commented out for testing
   
@@ -255,84 +254,56 @@ void CalculateBearing() {
     // worry about walls 1, 2, & 6
     
     // avoid wall 1
-    if(dists[0] < 5) {
+    if(dists[0] < 50) {
       bearing = 180;
-      Serial.print(dists[0]);
-      Serial.println("m to Wall 1");
     }
   
     // avoid wall 2
-    if(dists[1] < 5) {
+    if(dists[1] < 50) {
       bearing = 270;
-      Serial.print(dists[1]);
-      Serial.println("m to Wall 2");
     }
 
     // avoid wall 6
-    if(dists[5] < 5) {
+    if(dists[5] < 50) {
       bearing = 90;
-      Serial.print(dists[5]);
-      Serial.println("m to Wall 6");
     }
   
     // avoid wall 1 & 2
-    if(dists[0] < 5 && dists[1] < 5) {
+    if(dists[0] < 50 && dists[1] < 50) {
       bearing = 225;
-      Serial.print(dists[0]);
-      Serial.print("m to Wall 1, ");
-      Serial.print(dists[1]);
-      Serial.println("m to Wall 2");
     } 
 
     // avoid wall 1 & 6
-    if(dists[0] < 5 && dists[5] < 5) {
+    if(dists[0] < 50 && dists[5] < 50) {
       bearing = 135;
-      Serial.print(dists[0]);
-      Serial.print("m to Wall 1, ");
-      Serial.print(dists[5]);
-      Serial.println("m to Wall 6");
     }
   } else {
     // the robot's in the bottom section
     // worry about walls 3, 4, & 5
 
     // avoid wall 3
-    if(dists[2] < 5) {
+    if(dists[2] < 50) {
       bearing = 270;
-      Serial.print(dists[2]);
-      Serial.println("m to Wall 3");
     }
   
     // avoid wall 4
-    if(dists[3] < 5) {
+    if(dists[3] < 50) {
       bearing = 0;
-      Serial.print(dists[3]);
-      Serial.println("m to Wall 4");
     }
 
     // avoid wall 5
-    if(dists[4] < 5) {
+    if(dists[4] < 50) {
       bearing = 90;
-      Serial.print(dists[4]);
-      Serial.println("m to Wall 5");
     }
   
     // avoid wall 3 & 4
-    if(dists[2] < 5 && dists[3] < 5) {
+    if(dists[2] < 50 && dists[3] < 50) {
       bearing = 315;
-      Serial.print(dists[2]);
-      Serial.print("m to Wall 3, ");
-      Serial.print(dists[3]);
-      Serial.println("m to Wall 4");
     } 
 
     // avoid wall 4 & 5
-    if(dists[3] < 5 && dists[4] < 5) {
+    if(dists[3] < 50 && dists[4] < 50) {
       bearing = 45;
-      Serial.print(dists[3]);
-      Serial.print("m to Wall 4, ");
-      Serial.print(dists[4]);
-      Serial.println("m to Wall 5");
     }
   }
   
@@ -391,8 +362,8 @@ void CalculateSteering() {
     
   }
 
-  // x == ref angle, +/- 1
-  if(x >= bearing - 2 && x <= bearing + 2) {
+  // heading == ref angle, +/- 1
+  if(heading >= bearing - 2 && heading <= bearing + 2) {
     steeringAngle = 93; // go straight
   }
 }
@@ -400,7 +371,7 @@ void CalculateSteering() {
 void SetCarDirection() {   
   
   // Production speed
-  carSpeed = 10;
+  carSpeed = 15;
 
   // Test speed
   //carSpeed = 0;
@@ -442,13 +413,6 @@ void printDiagnoticsOnLCD() {
 }
 
 void loop() {
-  if(iterator == 10) {
-    Serial.print("Distance ");
-    Serial.print(dists[0]);
-    Serial.print(" Bearing ");
-    Serial.println(bearing);
-    iterator = 0;
-  }
   lcd.clear();      // clear LCD
   // you can pring anything on the LCD to debug your program while you're in the field!
   printDiagnoticsOnLCD();
